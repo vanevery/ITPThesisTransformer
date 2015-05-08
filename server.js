9ae565c63292911251b30b7ba03c2206d8ad6a62
vanevery <vanevery@walking-productions.com>
2015-05-08 10:25:59 -0400

    Handles two different client types, sending and receiving logic

--- server.js
+++ server.js
@@ -9,10 +9,14 @@ function requestHandler(req, res) {
 
 	var parsedUrl = url.parse(req.url);
 	console.log("The Request is: " + parsedUrl.pathname);
+
+	var path = parsedUrl.pathname;
+	if (path == "/") {
+		path = "/index.html";
+	}
 	
 	// Read index.html
-	
-	fs.readFile(__dirname + parsedUrl.pathname, 
+	fs.readFile(__dirname + path,
 		// Callback function for reading
 		function (err, data) {
 			// if there is an error
@@ -36,6 +40,10 @@ function requestHandler(req, res) {
 // WebSockets work with the HTTP server
 var io = require('socket.io').listen(httpServer);
 
+var adminClient = null;
+var broadcasterClients = [];
+var viewerClients = [];
+
 // Register a callback function to run when we have an individual connection
 // This is run for each individual user that connects
 io.sockets.on('connection', 
@@ -43,8 +51,26 @@ io.sockets.on('connection',
 	function (socket) {
 	
 		console.log("We have a new client: " + socket.id);
-	
-		// When this user "send" from clientside javascript, we get a "message"
+
+		socket.on('broadcaster',
+			function (data) {
+				broadcasterClients.push(socket);
+			}
+		);
+
+		socket.on('viewer',
+			function (data) {
+				viewerClients.push(socket);
+			}
+		);
+
+		socket.on('admin',
+			function (data) {
+				adminClient = socket;
+			}
+		);
+
+		/* Just an example	
 		// client side: socket.send("the message");  or socket.emit('message', "the message");
 		socket.on('message', 
 			// Run this function when a message is sent
@@ -59,18 +85,42 @@ io.sockets.on('connection',
 				// io.sockets.emit('message', "this goes to everyone");
 			}
 		);
-		
-		// When this user emits, client side: socket.emit('otherevent',some data);
+		*/
+	
+		// When we get an image from a broadcaster	
 		socket.on('image', function(data) {
 			// Data comes in as whatever was sent, including objects
-			console.log("Received: 'image' " + data.rotationMatrix);
-			socket.broadcast.emit('image',data);
+			//console.log("Received: 'image' " + data.rotationMatrix);
 			socket.emit('gotimage');
+
+			// Loop through the viewers, see if they are ready for it
+			for (var i = 0; i < viewerClients.length; i++) {
+				if (viewerClients[i] != null && viewerClients[i].sendnewimage) {
+					viewerClients[i].sendnewimage = false;
+					viewerClients[i].emit('image', data);
+				}
+			}
 		});
-		
-		
+
+		// Viewer client letting us know if they are ready for a new image
+		socket.on('sendnewimage', function (data) {
+			socket.sendnewimage = true;
+		});
+	
+		// Keep our arrays tidy, clean up disconnects	
 		socket.on('disconnect', function() {
 			console.log("Client has disconnected");
+			var indexToRemove = broadcasterClients.indexOf(socket);
+			if (indexToRemove > -1) {
+				var removedElements = broadcasterClients.splice(indexToRemove, 1);	
+				console.log("Removed " + removedElements.length + " elements from broadcaster array");
+			} else {
+				indexToRemove = viewerClients.indexOf(socket);
+				if (indexToRemove > -1) {
+					var removedElements = viewerClients.splice(indexToRemove, 1);	
+					console.log("Removed " + removedElements.length + " elements from viewer array");
+				}
+			}		
 		});
 	}
 );
